6c15b006d8838db91f28054638d6ea832010e765
hasnain haider alvi <160486646+Hasnainalvi@users.noreply.github.com>
2024-04-02 15:16:03 +0500

    adding a simple calculator

---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//printing a wellcome message
+console.log("\n\twellcome to \'hasnainhaideralvi\' - CLI Simple Calculator\n");
+// Asking Questions from Users through Inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstnumber" },
+    { message: "Enter Second Number", type: "number", name: "secondnumber" },
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+console.log(answers);
+//conditonal operators if-else
+if (answers.operator === "Addition") {
+    console.log(answers.firstnumber + answers.secondnumber);
+}
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstnumber - answers.secondnumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstnumber * answers.secondnumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstnumber / answers.secondnumber);
+}
+else {
+    console.log("Invalid Input");
+}
---
+++ index.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+import { Console } from "console";
+import inquirer from "inquirer";
+import { log } from "util";
+
+//printing a wellcome message
+
+console.log("\n\twellcome to \'hasnainhaideralvi\' - CLI Simple Calculator\n");
+
+
+// Asking Questions from Users through Inquirer
+
+let answers = await inquirer.prompt([
+{message:"Enter First Number",type: "number", name: "firstnumber"},
+{message: "Enter Second Number", type: "number", name: "secondnumber"},
+{
+    message:"Select one Operator to perform Operations",
+    type : "list",
+    name: "operator",
+    choices:["Addition","Subtraction","Multiplication","Division"],
+    }, 
+]);
+
+console.log(answers);
+
+//conditonal operators if-else
+
+if(answers.operator === "Addition"){
+ console.log(answers.firstnumber + answers.secondnumber)
+}
+else if(answers.operator === "Subtraction"){
+    console.log(answers.firstnumber - answers.secondnumber) 
+}
+else if (answers.operator === "Multiplication"){
+    console.log(answers.firstnumber * answers.secondnumber)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstnumber / answers.secondnumber)
+}
+else{
+   console.log("Invalid Input")
+}
